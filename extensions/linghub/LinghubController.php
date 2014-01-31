2c5530374756bac2438007436d99cabf946b817d
Natanael Arndt <arndtn@gmail.com>
2014-01-31 01:18:25 +0100

    Add preliminary support for adding datasets and some dataset view code

--- extensions/linghub/LinghubController.php
+++ extensions/linghub/LinghubController.php
@@ -18,7 +18,17 @@
  */
 class LinghubController extends OntoWiki_Controller_Component
 {
-    public function adddatasetAction(){
+    public function init ()
+    {
+        parent::init();
+        define('LH_DCTERMS_NS', 'http://purl.org/dc/terms/');
+        define('LH_FOAF_NS',    'http://xmlns.com/foaf/0.1/');
+        define('LH_VOID_NS',    'http://rdfs.org/ns/void#');
+        define('LH_LING_NS',    'http://lgd.aksw.org/LingHub/vocab/');
+        define('LH_PROV_NS',    'http://www.w3.org/ns/prov#');
+    }
+    public function adddatasetAction()
+    {
         // tells the OntoWiki to not apply the template to this action
         $this->_helper->viewRenderer->setNoRender();
         $this->_helper->layout->disableLayout();
@@ -68,7 +78,13 @@ class LinghubController extends OntoWiki_Controller_Component
             $newStatements = array();
             $newStatements[$datasetUri] = $newModel->getPO($datasetUri);
 
-            // TODO filter statements by a given application profile for void:Datasets
+            $newStatements = $this->_filterByApplicationProfile($newStatements);
+
+            $newStatements[$datasetUri][EF_RDF_TYPE][] = array(
+                'type' => 'uri', 'value' => LH_VOID_NS . 'Dataset'
+            );
+            // Add the found statements to our model
+            $model->addMultipleStatements($newStatements);
 
             $titles = $this->_getAllTitles($newStatements, $model);
 
@@ -89,7 +105,8 @@ class LinghubController extends OntoWiki_Controller_Component
         $this->_response->setBody(json_encode($result));
     }
 
-    private function _getAllTitles ($newStatements, $model) {
+    private function _getAllTitles ($newStatements, $model)
+    {
         $titleHelper = new OntoWiki_Model_TitleHelper($model);
         foreach ($newStatements as $resourceUri => $resourceDescription) {
             $titleHelper->addResource($resourceUri);
@@ -106,5 +123,52 @@ class LinghubController extends OntoWiki_Controller_Component
         }
 
         return $titles;
+    }
+
+    private function _filterByApplicationProfile ($statements, $type = null)
+    {
+        $applicationProfile = array(
+            EF_RDF_TYPE,
+            LH_DCTERMS_NS . 'title',
+            LH_DCTERMS_NS . 'description',
+            LH_DCTERMS_NS . 'license',
+            LH_DCTERMS_NS . 'created',
+            LH_DCTERMS_NS . 'issued',
+            LH_DCTERMS_NS . 'modified',
+            LH_DCTERMS_NS . 'subject',
+            LH_DCTERMS_NS . 'creator',
+            LH_DCTERMS_NS . 'publisher',
+            LH_DCTERMS_NS . 'contributor',
+            LH_DCTERMS_NS . 'relation',
+            LH_FOAF_NS . 'homepage',
+            LH_FOAF_NS . 'page',
+            LH_VOID_NS . 'feature',
+            LH_VOID_NS . 'triples',
+            LH_VOID_NS . 'rootResource',
+            LH_VOID_NS . 'exampleResource',
+            LH_VOID_NS . 'uriSpace',
+            LH_VOID_NS . 'sparqlEndpoint',
+            LH_VOID_NS . 'dataDump',
+            LH_LING_NS . 'language',
+            LH_LING_NS . 'version',
+            LH_LING_NS . 'latestVersion',
+            LH_LING_NS . 'links',
+            LH_LING_NS . 'subDataset',
+            LH_LING_NS . 'superDataset',
+            LH_PROV_NS . 'wasGeneratedBy',
+            LH_PROV_NS . 'wasDerivedFrom',
+        );
+
+        $filteredStatements = array();
+        foreach ($statements as $resourceUri => $description) {
+            $filteredStatements[$resourceUri] = array();
+            foreach ($description as $predicateUri => $properties) {
+                if (in_array($predicateUri, $applicationProfile)) {
+                    $filteredStatements[$resourceUri][$predicateUri] = $properties;
+                }
+            }
+        }
+
+        return $filteredStatements;
     }
 }
